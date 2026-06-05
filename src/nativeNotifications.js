201a02efdd93ebb12fdde040ba921d8a79bca699
Talha Turkman <notangryofficial@gmail.com>
2026-06-05 14:31:33 +0300

    v0.24.0: add shell.beep() on native notification to ensure sound on all PCs

--- src/nativeNotifications.js
+++ src/nativeNotifications.js
@@ -1,4 +1,4 @@
-const { BrowserWindow, ipcMain, screen } = require('electron');
+const { BrowserWindow, ipcMain, screen, shell } = require('electron');
 
 const OVERLAY_WIDTH = 280;
 const OVERLAY_HEIGHT = 56;
@@ -448,6 +448,7 @@ function createNativeNotificationService({
 
     activeNotifications.set(normalized.id, record);
     record.overlayWindow = createOverlayWindow(record);
+    shell.beep();
     writeLog('notification shown', {
       id: normalized.id,
       title: normalized.title,
